cfdfba61bde2013ce020a358715ca857421c01a0
hannahw1 <hannahw1@stanford.edu>
2018-02-22 16:44:36 -0800

    update DSSPFeaturizer test

--- msmbuilder/tests/test_dssp_featurizer.py
+++ msmbuilder/tests/test_dssp_featurizer.py
@@ -7,10 +7,16 @@ from msmbuilder.example_datasets import FsPeptide
 
 t = FsPeptide().get().trajectories[0][:10]
 
-def test_dssp_featurizer():
-    
+def test_dssp_featurizer(t):
+
+    residue_list = [x for x in t.topology.residues]
+    if "ACE"==residue_list[0].name: #protein has caps
+        n_residues = t.n_residues - 2
+    else:
+        n_residues = t.n_residues
+        
     value = DSSPFeaturizer(simplified=True).partial_transform(t)
-    assert value.shape == (t.n_frames, (t.n_residues - 2) * 3)
+    assert value.shape == (t.n_frames, n_residues * 3), # simple DSSP should have 3 classes per residue
 
     value = DSSPFeaturizer(simplified=False).partial_transform(t)
-    assert value.shape == (t.n_frames, (t.n_residues - 2) * 8)
+    assert value.shape == (t.n_frames, n_residues * 8), # full DSSP should have 8 classes per residue
